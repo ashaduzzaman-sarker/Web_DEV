d2cddd04f50d9442559a650082e742b4ab934126
Ashaduzzaman Sarker <ashaduzzaman2505@gmail.com>
2026-05-21 16:28:09 +0000

    Fixed capitalization in problem comments and added checkType function

--- B7A1/solutions.ts
+++ B7A1/solutions.ts
@@ -1,9 +1,20 @@
-// problem 1 Solution
+// Problem 1 Solution
 const filterEvenNumbers = (ar: number[]): number[] => {
   return ar.filter((num) => num % 2 === 0);
 };
 
-// problem 2 Solution
+// Problem 2 Solution
 const reverseString = (str: string): string => {
   return str.split('').reverse().join('');
+};
+
+// Problem 3 Solution
+type StringOrNumber = string | number;
+
+const checkType = (value: StringOrNumber): string => {
+  if (typeof value === 'string') {
+    return 'String';
+  } else {
+    return 'Number';
+  }
 };
